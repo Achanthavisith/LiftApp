f3f828eff9f24b1566419039ef257de07e981f10
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-05-09 18:12:57 -0400

    start

---
+++ screens/ExerciseCategory.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const ExerciseCategory = (data) => {
+  return (
+      <SafeAreaView>
+            <View>
+                <Text>ExerciseCategory</Text>
+            </View>
+      </SafeAreaView>
+  )
+}
+
+export default ExerciseCategory

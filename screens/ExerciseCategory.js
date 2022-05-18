cc4b9560d1bb532f12deb2edc96614186ac8f71f
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-05-17 21:13:29 -0400

    pushing navigation to exercise

--- screens/ExerciseCategory.js
+++ screens/ExerciseCategory.js
@@ -1,12 +1,12 @@
 import { View, Text, Appearance, StyleSheet } from 'react-native'
 import React from 'react'
 
-import { Colors } from '../styles/theme'
 import CategoryCard from '../components/CategoryCard';
 
 const colorScheme = Appearance.getColorScheme();
 
 const ExerciseCategory = ( {data} ) => {
+
   return (
     <View>
       <View >
@@ -15,27 +15,5 @@ const ExerciseCategory = ( {data} ) => {
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-
-  white: {
-    color: 'white',
-  },
-
-  black: {
-    color: 'black'
-  }
-});
 
 export default ExerciseCategory

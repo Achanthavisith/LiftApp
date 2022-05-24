14656264f91c6662d7f36e3bf96806d127a4fc14
golirra <webbseamus2@gmail.com>
2022-05-23 19:46:37 -0700

    exercise category image+filepath

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,54 +1,61 @@
-import { View, Text, TouchableOpacity, StyleSheet  } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
-import React from 'react'
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
 
-import { Colors, Sizes, Fonts } from "../styles/theme"
+import { Colors, Sizes, Fonts } from "../styles/theme";
 
-const CategoryCard = ( {data} ) => {
+const CategoryCard = ({ data }) => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Exercises", { categoryId: data.id })}
+    >
+      <View style={styles.exerciseProp}>
+        <View>
+          <Image
+            style={styles.image}
+            source={require("../assets/muscle-vector-" +
+              data.name.toLowerCase() +
+              ".png")}
+          />
+        </View>
 
-    return (
-        <TouchableOpacity 
-            onPress={()=> 
-                navigation.navigate(
-                    "Exercises", 
-                    {categoryId: data.id})
-            }
-        >
-            <View style={styles.exerciseProp}>
-                <View>
+        <Text style={styles.light}>{data.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-                </View>
-        
-                <Text style={styles.light}>
-                    {data.name}
-                </Text>
-            </View>
-        </TouchableOpacity>
-    )
-}
+const styles = StyleSheet.create({
+  light: {
+    textAlign: "center",
+    color: Colors.blue,
+    fontSize: Fonts.small,
+    fontWeight: "bold",
+  },
 
-const styles = StyleSheet.create ({
-    light: {
-        textAlign: 'center',
-        color: Colors.blue,
-        fontSize: Fonts.small,
-        fontWeight: 'bold'
-    }, 
+  exerciseProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    backgroundColor: Colors.almond,
+    padding: Sizes.large,
+    margin: Sizes.large,
+    height: 120,
+    justifyContent: "center",
+    borderRadius: Sizes.large,
+  },
+  image: {
+    justifyContent: "center",
+    height: 100,
+    width: 70,
+    borderRadius: 2,
+    padding: 1,
+    alignSelf: "baseline",
+    position: "absolute",
+  },
+});
 
-    exerciseProp: {
-        shadowColor: '#171717',
-        shadowOffset: {width: -2, height: 4},
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        backgroundColor: Colors.almond,
-        padding: Sizes.large, 
-        margin : Sizes.large,
-        height: 120,
-        justifyContent: 'center',
-        borderRadius: Sizes.large, 
-    },
-})
-
-export default CategoryCard
+export default CategoryCard;
